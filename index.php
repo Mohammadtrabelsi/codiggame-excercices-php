aa0ad501b3d60f7acac3f0eb5438ef81b3328e2d
Mohammad Trabelsi <mohammad.trabelsi@gmail.com>
2023-11-10 10:08:26 +0100

    update UI

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php include 'parts/headart.php'; ?>
+<h1>Directory contents</h1>
+<ul class="list-group list-group-flush">
+    <?php
+    // Define the directory path
+    $dir = dirname(__FILE__);
 
-        <h1> PHP coding game exercices with solution</h1>
-        <ul>
-            <?php
-            // Define the directory path
-            $dir = dirname(__FILE__);
+    // Get an array of the directory contents using scandir()
+    $files = scandir($dir);
 
-            // Get an array of the directory contents using scandir()
-            $files = scandir($dir);
-
-            // Output the contents of the directory using print_r()
-            echo "Directory contents: \n";
-            foreach ($files as $file) {
-                if (!in_array($file, ['index.php', '.', '..', '.idea', 'parts']))
-                    echo '<a href="' . $file . '"><li>' . $file . '</li></a>';
-            }
-            ?>
-        </ul>
+    // Output the contents of the directory using print_r()
+    foreach ($files as $file) {
+        if (!in_array($file, ['index.php', '.', '.git', '..', '.idea', 'parts']))
+            echo '<a href="' . $file . '"><li  class="list-group-item" >' . $file . '</li></a>';
+    }
+    ?>
+</ul>
 <?php include 'parts/footerPart.php'; ?>
